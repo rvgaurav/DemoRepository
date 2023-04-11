e4cdb84eb8d1ad7f07e4f0451d95c0839e31e6d2
rvgaurav <37259734+rvgaurav@users.noreply.github.com>
2023-04-11 22:26:03 +0530

    Initial commit

---
+++ src/app/team.ts
@@ -0,0 +1,28 @@
+export interface Teams{
+  id : number ; 
+  name : string ;
+  city : string ; 
+  rankings : number;
+}
+
+export const teams = [
+  {
+    id : 1,
+    name : "Mumbai Indians",
+    city : 'Mumbai',
+    rankings : 1
+  },
+  {
+    id : 2,
+    name : "Kolkata Knight Riders",
+    city : 'Kolkata',
+    rankings : 2
+  },
+  {
+    id : 3,
+    name : "Chennai Super Kings",
+    city : 'Chennai,
+    rankings : 2
+  }
+
+]
